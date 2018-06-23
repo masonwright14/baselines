1f5d54a8f1d25d7ce0045231c011e4867659a011
Mason Wright <masonwright14@gmail.com>
2018-06-23 18:45:55 -0400

    adding stub for new method of retraining

--- baselines/deepq/__init__.py
+++ baselines/deepq/__init__.py
@@ -3,7 +3,7 @@ from baselines.deepq.build_graph import build_act, build_train  # noqa
 # from baselines.deepq.simple import learn, load  # noqa
 from baselines.deepq.simple_conservative import learn, load, load_for_multiple_nets, \
     learn_multiple_nets, learn_and_save, load_with_scope, \
-    load_for_multiple_nets_with_scope
+    load_for_multiple_nets_with_scope, retrain_and_save
 from baselines.deepq.replay_buffer import ReplayBuffer, PrioritizedReplayBuffer  # noqa
 
 def wrap_atari_dqn(env):
